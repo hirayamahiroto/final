01a663852cd9c2234078c8a866bd014fae0171e2
hirayamahiroto <h.hirayama@ei-shin.com>
2023-04-28 17:25:00 +0900

    tableを作成しました

---
+++ database/migrations/2023_04_28_080016_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("companies", function (Blueprint $table) {
+            $table->id();
+            $table
+                ->string("name")
+                ->nullable(false)
+                ->default("〜〜〜会社");
+            $table->string("email")->unique();
+            $table->timestamp("email_verified_at")->nullable();
+            $table
+                ->string("human_name")
+                ->nullable(false)
+                ->default("社長");
+            $table->string("password");
+            //外部とやり寄りするため、nullable
+            $table->foreignId("industry_id")->nullable()->constrained("industries")->cascadeOnDelete();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("companies");
+    }
+};
